c595221337cbab67fcd006a497db364241b05a77
Raihan Taher <raihan.taher@gmail.com>
2015-10-03 16:47:14 +0600

    a query function added to Database class

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -2,9 +2,34 @@
 
 require_once("database_config.php");
 
-$con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+class Database {
 
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    private $conn;
+
+    function __construct(){
+
+        $this->conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+        if (!$this->conn)
+        {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+    }
+
+    public function query(){
+        $result = mysqli_query($this->conn, "SELECT * FROM users");
+
+        return $result;
+    }
+
 }
+
+$database = new Database;
+
+$res = $database->query();
+
+while($row = mysqli_fetch_array($res)){
+    echo $row['username'];
+}
+
