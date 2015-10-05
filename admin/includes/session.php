9edea3ae9ed1adc990fc05ddeaf3b71c7849b523
Raihan Taher <raihan.taher@gmail.com>
2015-10-05 13:59:19 +0600

    added a logout method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,11 +2,31 @@
 
 class Session {
 
-    private $signed_in;
+    private $signed_in = false;
     public $user_id;
 
     function __construct(){
         session_start();
+        $this->check_login();
+    }
+
+    private function is_signed_in(){
+        return $this->signed_in;
+    }
+
+    public function login($user){
+
+        if($user){
+
+            $this->user_id = $_SESSION['user_id'] = $user->id;
+            $this->signed_in = true;
+        }
+    }
+
+    public function logout() {
+        unset($_SESSION['user_id']);
+        unset($user_id);
+        $this->signed_in = false;
     }
 
     private function check_login(){
