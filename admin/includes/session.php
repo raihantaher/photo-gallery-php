7ce5e57edfdfa9eb0de26ae01f3884621105b243
Raihan Taher <raihan.taher@gmail.com>
2015-10-05 13:30:28 +0600

    added a check_login method in session class

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,11 +2,22 @@
 
 class Session {
 
-    private $sign_in;
+    private $signed_in;
     public $user_id;
 
     function __construct(){
         session_start();
+    }
+
+    private function check_login(){
+
+        if(isset($_SESSION['user_id'])){
+            $this->user_id = $_SESSION['user_id'];
+            $this->signed_in = true;
+        } else{
+            unset($this->user_id);
+            $this->signed_in = false;
+        }
     }
 
 }
